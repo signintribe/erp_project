e0287eee988bd0582b859050cf113c7418bed633
waqaskhan005 <wiki563822@gmail.com>
2021-04-24 16:12:19 +0500

    commit vendor adresss

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,7 @@ Route::group(['prefix'=>'vendor'], function () {
   Route::get('vendor-person', 'VendorController@contact_person')->name('organization-contact-person');
   Route::resource('maintain-vendor-information', 'Admin\VendorInformationController');
   Route::resource('maintain-vendor-address', 'Admin\VendorAddressController');
+  
 });
 
 Route::get('getAddress/{address_id}', 'Admin\EmployeeAddressController@getAddress');
