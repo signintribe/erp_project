e0405c85f8173bf12ef9f4e8a9ae7e11e8c0c207
WaseemHashmi <waseemhashmi171@gmail.com>
2021-05-24 18:12:19 +0500

    save

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -8,6 +8,14 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\tblproduct_informations;
+use App\Models\tblproduct_pricing;
+use App\Models\tblproduct_attributes;
+use App\Models\tblproduct_vendor;
+use App\Models\tblproduct_stockavailability;
+use App\Models\tblproduct_accounts;
+use Auth;
+use DB;
 /**
  * Description of InventoryController
  *
@@ -28,8 +36,26 @@ class InventoryController extends Controller{
 
     public function saveInventory(Request $request)
     {
-        $data = $request->all();
-        $attribute = json_decode($data['attributes'], true);
-        return $attribute;
+        $product = $request->except('stock_sale_order','attribute_name','vendor_name','stock_class','product_status','stock_in_hand','store_name','reorder_quantity','stock_pur_order','chartof_account_cost','chartof_account_inventory','chartof_account_sale','product_id','income_tax','withholding_tax','sales_tax','fed','import_duty','tax_adjustment','tax_exemption','delivery_charges','gross_pur_price','carriage_inward_charges','octri_taxes','net_pur_price',);
+        $attributes = $request->except('stock_sale_order','product_name','product_description','category_id','vendor_name','stock_class','product_status','stock_in_hand','store_name','reorder_quantity','stock_pur_order','chartof_account_cost','chartof_account_inventory','chartof_account_sale','income_tax','withholding_tax','sales_tax','fed','import_duty','tax_adjustment','tax_exemption','delivery_charges','gross_pur_price','carriage_inward_charges','octri_taxes','net_pur_price',);
+        $pricing = $request->except('stock_sale_order','product_name','product_description','category_id','attribute_name','vendor_name','stock_class','product_status','stock_in_hand','store_name','reorder_quantity','stock_pur_order','chartof_account_cost','chartof_account_inventory','chartof_account_sale',);
+        $vendor = $request->except('stock_sale_order','product_name','product_description','category_id','attribute_name','stock_in_hand','store_name','reorder_quantity','stock_pur_order','chartof_account_cost','chartof_account_inventory','chartof_account_sale','income_tax','withholding_tax','sales_tax','fed','import_duty','tax_adjustment','tax_exemption','delivery_charges','gross_pur_price','carriage_inward_charges','octri_taxes','net_pur_price',);
+        $stock = $request->except('product_name','product_description','category_id','attribute_name','vendor_name','stock_class','product_status','chartof_account_cost','chartof_account_inventory','chartof_account_sale','income_tax','withholding_tax','sales_tax','fed','import_duty','tax_adjustment','tax_exemption','delivery_charges','gross_pur_price','carriage_inward_charges','octri_taxes','net_pur_price',);
+        $account = $request->except('stock_sale_order','product_name','product_description','category_id','attribute_name','vendor_name','stock_class','product_status','stock_in_hand','store_name','reorder_quantity','stock_pur_order','income_tax','withholding_tax','sales_tax','fed','import_duty','tax_adjustment','tax_exemption','delivery_charges','gross_pur_price','carriage_inward_charges','octri_taxes','net_pur_price',);
+        $product['user_id'] = Auth::user()->id;
+        tblproduct_informations::create($product);
+        $attributes['product_id'] = $product->id;
+        tblproduct_attributes::create($attributes);
+        $pricing['product_id'] = $product->id;
+        tblproduct_pricing::create($pricing);
+        $vendor['product_id'] = $product->id;
+        tblproduct_vendor::create($vendor);
+        $stock['product_id'] = $product->id;
+        tblproduct_stockavailability::create($stock);
+        $account['product_id'] = $product->id;
+        tblproduct_accounts::create($account);
+        
+
+        return 'Successfully Add Har Cheez';
     }
 }
