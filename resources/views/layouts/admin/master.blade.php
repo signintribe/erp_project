0b95fe61af50483aa2cd0b0eea668e1abf50ee04
signintribe <m_attiquakram@yahoo.com>
2021-08-06 15:47:03 +0500

    add new theme admin lte

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -26,20 +26,37 @@
             <!-- partial:partials/_navbar.html -->
             <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row d-print-none">
                 <div class="navbar-menu-wrapper d-flex align-items-stretch justify-content-between">
-                    <ul class="navbar-nav mr-lg-2 d-none d-lg-flex">
+                    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+                        <a class="navbar-brand brand-logo" href="{{ url('adminhome') }}"><img src="{{ asset('public/dashboard/images/logo.jpg') }}" alt="logo"/></a>
+                        <a class="navbar-brand brand-logo-mini" href="{{ url('adminhome') }}"><img src="{{ asset('public/dashboard/images/logo.jpg') }}" alt="logo"/></a>
+                    </div>
+                    <ul class="navbar-nav mr-auto">
                         <li class="nav-item nav-toggler-item">
                             <button class="navbar-toggler align-self-center" type="button" data-toggle="minimize">
                                 <span class="mdi mdi-menu"></span>
                             </button>
                         </li>
-                        <li class="nav-item nav-search d-none d-lg-flex">
-                            
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Creation Tier
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Task Tier
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                Report Tier
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                User Auth Tier
+                            </a>
                         </li>
                     </ul>
-                    <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                        <a class="navbar-brand brand-logo" href="{{ url('adminhome') }}"><img src="{{ asset('public/dashboard/images/logo.jpg') }}" alt="logo"/></a>
-                        <a class="navbar-brand brand-logo-mini" href="{{ url('adminhome') }}"><img src="{{ asset('public/dashboard/images/logojpg') }}" alt="logo"/></a>
-                    </div>
                     <ul class="navbar-nav navbar-nav-right">
                         <li class="nav-item dropdown">
                             <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
